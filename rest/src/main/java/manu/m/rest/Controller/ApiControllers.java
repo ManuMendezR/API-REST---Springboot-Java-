bdb14a7a5d60ad513697f2821ca4809879ba3d3a
ManuMendezR <lerixionix457@gmail.com>
2025-09-13 19:40:32 +0200

    feat: creado controller con un comportamiento por defecto basico

--- rest/src/main/java/manu/m/rest/Controller/ApiControllers.java
+++ rest/src/main/java/manu/m/rest/Controller/ApiControllers.java
@@ -1,5 +1,14 @@
 package manu.m.rest.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiControllers {
+
+    @GetMapping("/")
+    public String getPage(){
+        return "Welcome";
+    }
 
 }
