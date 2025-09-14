bc8be2d3262eaae9edb2dbf57aba1cc0382867d4
ManuMendezR <lerixionix457@gmail.com>
2025-09-14 23:07:35 +0200

    feat: primera versión modelo service

---
+++ rest/src/main/java/manu/m/rest/Model/Service.java
@@ -0,0 +1,47 @@
+package manu.m.rest.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column 
+    private String nombreServicio;
+
+    @Column
+    private double precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+}
